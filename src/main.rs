602c8a0fe995ddd5683475f8f45f92c11a1f75b3
Ch1n3du <danielonyesoh@gmail.com>
2022-03-24 18:01:53 +0100

    Test commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-mod chunk;
-mod chunk_type;
-mod png;
-//mod commands;
-//mod args;
+// mod chunk;
+// mod chunk_type;
+// mod png;
+// //mod commands;
+// //mod args;
 
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
+// pub type Error = Box<dyn std::error::Error>;
+// pub type Result<T> = std::result::Result<T, Error>;
 
-use chunk_type::ChunkType;
+// use chunk_type::ChunkType;
+
+// fn main() {
+//     let _x = ChunkType::try_from([1, 2, 3, 4]);
+
+// }
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    let _x = ChunkType::try_from([1, 2, 3, 4]);
+    let args = Args::parse();
 
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
